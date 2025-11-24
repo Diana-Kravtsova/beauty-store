4ac1f6e1c272245f13a1f4ad108e258671fa2f8f
Diana-Kravtsova <dayana.kravtsova@gmail.com>
2025-11-24 22:04:52 +0300

    add(MUI): init Material UI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import './styles/variables.scss';
 import { Link, Outlet } from 'react-router';
+import Button from '@mui/material/Button';
+import LinearProgress  from '@mui/material/LinearProgress';
+import Box  from '@mui/material/Box';
+import './styles/variables.scss';
 
 export const App = () =>{
   return (
     <div>
       <Link to="/login">Login</Link>
       <Link to="/products">Products</Link>
+      <Button variant="contained">Hello world</Button>
+      <Box sx={{ width: '100%' }}>
+        <LinearProgress color="secondary" />
+      </Box>
       <Outlet/>
     </div>
   )
